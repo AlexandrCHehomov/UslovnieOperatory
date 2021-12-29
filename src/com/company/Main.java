674fc084109e99871e185b5a8712a305b01206c8
AlexCH <alexvilah@mail.ru>
2021-12-29 21:20:42 +0500

    Условные операторы дз выполненое

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main<clientDeviceYear, clientOS, system> {
     public static void main(String[] args) {
 	// 1
    int clientOS = 1;
-   if (clientOS >= 1) {
+   if (clientOS == 0) {
        System.out.println("Установите версию приложения для Android по ссылке");
-   } if (clientOS < 1) {
+   } if (clientOS == 1) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
 
